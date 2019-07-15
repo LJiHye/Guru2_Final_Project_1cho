cae26a814be790a3e3be7a1752bd54afee0ff85e
KimJiYeon <kwldusrnclf@naver.com>
2019-07-15 16:21:54 +0900

    BuyWriteActivity 스피너 수정

--- app/src/main/java/com/example/cho1/guru2_final_project_1cho/activity/BuyWriteActivity.java
+++ app/src/main/java/com/example/cho1/guru2_final_project_1cho/activity/BuyWriteActivity.java
@@ -13,8 +13,10 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -66,6 +68,17 @@ public class BuyWriteActivity extends AppCompatActivity {
             }
         });
 
+        //카테고리 드롭다운 스피너 추가
+        Spinner dropdown = (Spinner)findViewById(R.id.spinner1);
+        String[] items = new String[]{"옷", "책", "생활물품", "기프티콘", "데이터", "대리 예매", "전자기기", "화장품", "기타"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+
+        //제품상태 드롭다운 스피너 추가
+        Spinner dropdown2 = (Spinner)findViewById(R.id.spinner2);
+        String[] items2 = new String[]{"상", "중", "하"};
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items2);
+        dropdown2.setAdapter(adapter2);
 
     }  //end onCreate()
 
