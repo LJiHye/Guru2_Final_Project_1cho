94f6105399d5ae207cc2e48b64bce8dc3d6e400e
LJiHye <star4266@naver.com>
2019-07-15 12:59:21 +0900

    첫 커밋

---
+++ app/src/main/java/com/example/cho1/guru2_final_project_1cho/fragment/ExFragment.java
@@ -0,0 +1,16 @@
+package com.example.cho1.guru2_final_project_1cho.fragment;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cho1.guru2_final_project_1cho.R;
+
+public class ExFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_ex);
+    }
+}
