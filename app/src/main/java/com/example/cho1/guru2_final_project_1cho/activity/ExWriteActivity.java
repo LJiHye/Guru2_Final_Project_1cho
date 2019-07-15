67dd20eb2125e28b1047ded3f05f64eeb724d320
glala981 <eogkr2017@daum.net>
2019-07-15 16:42:10 +0900

    물물교환 글작성및 수정

--- app/src/main/java/com/example/cho1/guru2_final_project_1cho/activity/ExWriteActivity.java
+++ app/src/main/java/com/example/cho1/guru2_final_project_1cho/activity/ExWriteActivity.java
@@ -1,16 +1,237 @@
 package com.example.cho1.guru2_final_project_1cho.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.cho1.guru2_final_project_1cho.R;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ExWriteActivity extends AppCompatActivity {
+
+    private ImageView mImgItem;
+    private EditText mEdtTitle;
+    private  EditText mEdtItem;
+    private EditText mEdtBuyDate;
+    private EditText mEdtExpDate;
+    private EditText mEdtDefect;
+    private EditText mEdtSize;
+    private Spinner mSprState;
+
+    // 사진이 저장되는 경로
+    private Uri mCaptureUri;
+    //사진이 저장된 단말기상의 실제 경로
+    public String mPhotoPath;
+    //startActivityForResult() 에 넘겨주는 값, 이 값이 나중에 onActivityResult()로 돌아와서
+    //내가 던진값인지를 구별할 때 사용하는 상수.
+    public static final int REQUEST_IMAGE_CAPTURE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ex_write);
+
+        //카메라를 사용하기 위한 퍼미션을 요청한다.
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+        }, 0);
+
+        mImgItem = findViewById(R.id.imgItem);
+        mEdtTitle = findViewById(R.id.edtTitle);
+        mEdtItem = findViewById(R.id.edtItem);
+        mEdtBuyDate = findViewById(R.id.edtBuyDate);
+        mEdtExpDate = findViewById(R.id.edtExpDate);
+        mEdtDefect = findViewById(R.id.edtDefect);
+        mEdtSize = findViewById(R.id.edtSize);
+        mSprState = findViewById(R.id.sprState);
+
+        mImgItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takePicture();
+            }
+        });
+
+        findViewById(R.id.btnModify).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        //제품상태 드롭다운 스피너 추가
+        Spinner dropdown = (Spinner)findViewById(R.id.sprState);
+        String[] states = new String[]{"상", "중", "하"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, states);
+        dropdown.setAdapter(adapter);
+
+
+    } // onCreate()
+
+
+    private void takePicture() {
+
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            mCaptureUri = Uri.fromFile( getOutPutMediaFile() );
+        } else {
+            mCaptureUri = FileProvider.getUriForFile(this,
+                    "com.example.cho1.guru2_final_project_1cho", getOutPutMediaFile());
+        }
+
+        i.putExtra(MediaStore.EXTRA_OUTPUT, mCaptureUri);
+        startActivityForResult(i, REQUEST_IMAGE_CAPTURE);
+
+    }
+
+    private File getOutPutMediaFile() {
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "cameraDemo");
+        if(!mediaStorageDir.exists()) {
+            if(!mediaStorageDir.mkdirs()) {
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File file = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
+
+        mPhotoPath = file.getAbsolutePath();
+
+        return file;
+    }
+
+    private void sendPicture() {
+        Bitmap bitmap = BitmapFactory.decodeFile(mPhotoPath);
+        Bitmap resizedBmp = getResizedBitmap(bitmap, 4, 100, 100);
+
+        bitmap.recycle();
+
+        //사진이 캡쳐되서 들어오면 뒤집어져 있다. 이애를 다시 원상복구 시킨다.
+        ExifInterface exif = null;
+        try {
+            exif = new ExifInterface(mPhotoPath);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        int exifOrientation;
+        int exifDegree;
+        if(exif != null) {
+            exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_NORMAL);
+            exifDegree = exifOrientToDegree(exifOrientation);
+        } else {
+            exifDegree = 0;
+        }
+        Bitmap rotatedBmp = roate(resizedBmp, exifDegree);
+        mImgItem.setImageBitmap( rotatedBmp );
+        //줄어든 이미지를 다시 저장한다
+        saveBitmapToFileCache(resizedBmp, mPhotoPath);
+
+        Toast.makeText(this, "사진경로: " + mPhotoPath, Toast.LENGTH_LONG).show();
+    }
+
+    private void saveBitmapToFileCache(Bitmap bitmap, String strFilePath) {
+
+        File fileCacheItem = new File(strFilePath);
+        OutputStream out = null;
+
+        try
+        {
+            fileCacheItem.createNewFile();
+            out = new FileOutputStream(fileCacheItem);
+
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                out.close();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private int exifOrientToDegree(int exifOrientation) {
+        if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
+            return 90;
+        }
+        else if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
+            return 180;
+        }
+        else if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
+            return 270;
+        }
+        return 0;
+    }
+
+    private Bitmap roate(Bitmap bmp, float degree) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        return Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(), bmp.getHeight(),
+                matrix, true);
+    }
+
+    //비트맵의 사이즈를 줄여준다.
+    public static Bitmap getResizedBitmap(Bitmap srcBmp, int size, int width, int height){
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = size;
+        Bitmap resized = Bitmap.createScaledBitmap(srcBmp, width, height, true);
+        return resized;
+    }
+
+    public static Bitmap getResizedBitmap(Resources resources, int id, int size, int width, int height){
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = size;
+        Bitmap src = BitmapFactory.decodeResource(resources, id, options);
+        Bitmap resized = Bitmap.createScaledBitmap(src, width, height, true);
+        return resized;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //카메라로부터 오는 데이터를 취득한다.
+        if(resultCode == RESULT_OK) {
+            if(requestCode == REQUEST_IMAGE_CAPTURE) {
+                sendPicture();
+            }
+        }
     }
 }
