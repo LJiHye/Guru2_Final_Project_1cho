851dd4ad7c2f701a3b4236b643d85d9d5bc40725
KimJiYeon <kwldusrnclf@naver.com>
2019-07-15 14:00:07 +0900

    flea activity에 따른 activity와 fragment

---
+++ app/src/main/java/com/example/cho1/guru2_final_project_1cho/BuyWriteActivity.java
@@ -0,0 +1,14 @@
+package com.example.cho1.guru2_final_project_1cho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BuyWriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy_write);
+    }
+}
---
+++ app/src/main/java/com/example/cho1/guru2_final_project_1cho/FleaActivity.java
@@ -0,0 +1,74 @@
+package com.example.cho1.guru2_final_project_1cho;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class FleaActivity extends AppCompatActivity {
+
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flea);
+        mTabLayout = findViewById(R.id.tabLayout);
+        mViewPager = findViewById(R.id.viewPager);
+
+        //탭 생성
+        mTabLayout.addTab(mTabLayout.newTab().setText("메모"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("회원 정보"));
+        mTabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        //ViewPager 생성
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), mTabLayout.getTabCount());
+        //Tab이랑 viewpager랑 연결
+        mViewPager.setAdapter(adapter);
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+        mTabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                mViewPager.setCurrentItem(tab.getPosition());
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) { }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) { }
+        });
+
+    }
+
+    static class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private int tabCount;
+
+        public ViewPagerAdapter(FragmentManager fm, int count){
+            super(fm);
+            this.tabCount = count;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    return new FragmentBuy();
+                case 1:
+                    return new FragmentSell();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return tabCount; } //실수하면 안됨! 만들어 놓은걸로 바꿔야 함
+
+
+    }
+}
