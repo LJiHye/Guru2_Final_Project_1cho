790e894b4b5bef81a4a65264fd8c712ed4d54b52
omneu <heaju_724@naver.com>
2019-07-15 16:22:08 +0900

    ddd

---
+++ app/src/main/java/com/example/cho1/guru2_final_project_1cho/bean/MemberBean.java
@@ -0,0 +1,14 @@
+package com.example.cho1.guru2_final_project_1cho.bean;
+
+import java.util.List;
+
+//회원정보 Bean
+public class MemberBean {
+
+    public String photoPath;
+    public String memId;
+    public String memName;
+    public String memPw;
+    public String memKakaoId;
+
+}
---
+++ app/src/main/java/com/example/cho1/guru2_final_project_1cho/fragment/FragmentFlea.java
@@ -0,0 +1,16 @@
+package com.example.cho1.guru2_final_project_1cho.fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cho1.guru2_final_project_1cho.R;
+
+public class FragmentFlea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_flea);
+    }
+}
