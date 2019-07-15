03aa53b8809165482e319ae130b7946c3ce93b37
Your Name <you@example.com>
2019-07-15 16:46:29 +0900

    팔아주세요 activity/xml(activity_sell_modify/activity_sell_write/activity_sell_item) 추가

--- app/src/main/java/com/example/cho1/guru2_final_project_1cho/activity/SellModifyActivity.java
+++ app/src/main/java/com/example/cho1/guru2_final_project_1cho/activity/SellModifyActivity.java
@@ -3,6 +3,8 @@ package com.example.cho1.guru2_final_project_1cho.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.cho1.guru2_final_project_1cho.R;
 
@@ -12,5 +14,12 @@ public class SellModifyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sell_modify);
+
+        //카테고리 드롭다운 스피너 추가
+        Spinner dropdown = (Spinner)findViewById(R.id.spinner1);
+        String[] items = new String[]{"옷", "책", "생활물품", "기프티콘", "데이터", "대리 예매", "전자기기", "화장품", "기타"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+
     }
 }
