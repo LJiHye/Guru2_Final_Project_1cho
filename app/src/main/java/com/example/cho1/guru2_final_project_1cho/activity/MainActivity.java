c06ba5fae680a1da997a6c51ab137c088bc5da89
LJiHye <star4266@naver.com>
2019-07-15 13:36:32 +0900

    메인화면

--- app/src/main/java/com/example/cho1/guru2_final_project_1cho/activity/MainActivity.java
+++ app/src/main/java/com/example/cho1/guru2_final_project_1cho/activity/MainActivity.java
@@ -1,16 +1,97 @@
 package com.example.cho1.guru2_final_project_1cho.activity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.cho1.guru2_final_project_1cho.R;
+import com.example.cho1.guru2_final_project_1cho.fragment.FragmentEx;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTabLayout = findViewById(R.id.tabLayout);
+        mViewPager = findViewById(R.id.viewPager);
+
+        // 탭 생성
+        mTabLayout.addTab(mTabLayout.newTab().setText("플리마켓"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("물물교환"));
+        mTabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        // ViewPager 생성
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), mTabLayout.getTabCount());
+        // 탭과 뷰페이저를 서로 연결
+        mViewPager.setAdapter(adapter);
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                mViewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) { }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) { }
+        });
+    } // end onCreate()
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private int tabCount;
+
+        public ViewPagerAdapter(FragmentManager fm, int count) {
+            super(fm);
+            this.tabCount = count;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch(position) {
+                case 0:
+                    //return new FragmentFlea();
+                case 1:
+                    return new FragmentEx();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return tabCount;
+        }
+    } //end class ViewPagerAdapter
+
+    private long backPressedAt;
+
+    @Override
+    public void onBackPressed() {
+        if (backPressedAt + TimeUnit.SECONDS.toMillis(2) > System.currentTimeMillis()) {
+            super.onBackPressed();
+            finish();
+        }
+        else {
+            if(this instanceof MainActivity) {
+                Toast.makeText(this, "한번더 뒤로가기 클릭시 앱을 종료 합니다.", Toast.LENGTH_SHORT).show();
+                backPressedAt = System.currentTimeMillis();
+            } else {
+                super.onBackPressed();
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/cho1/guru2_final_project_1cho/fragment/FragmentEx.java
@@ -0,0 +1,30 @@
+package com.example.cho1.guru2_final_project_1cho.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.cho1.guru2_final_project_1cho.R;
+
+public class FragmentEx extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_ex, container, false);
+
+        ImageView imgEx = view.findViewById(R.id.imgEx);
+        TextView txtExTitle = view.findViewById(R.id.txtExTitle);
+        TextView txtExSubTitle = view.findViewById(R.id.txtExSubTitle);
+        TextView txtExPrice = view.findViewById(R.id.txtExPrice);
+        TextView txtExDate = view.findViewById(R.id.txtExDate);
+
+        return view;
+    }
+}
