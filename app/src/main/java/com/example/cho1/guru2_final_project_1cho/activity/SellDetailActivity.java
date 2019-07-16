821573774a12d46ac9f1d5b107a3f94fbafd5b20
HeoYunJeong <yund_272@naver.com>
2019-07-16 16:53:15 +0900

    SellDetailActivity 수정중 사주세요, 팔아주세요 디테일 통일

--- app/src/main/java/com/example/cho1/guru2_final_project_1cho/activity/SellDetailActivity.java
+++ app/src/main/java/com/example/cho1/guru2_final_project_1cho/activity/SellDetailActivity.java
@@ -3,10 +3,12 @@ package com.example.cho1.guru2_final_project_1cho.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.TextView;
 
 import com.example.cho1.guru2_final_project_1cho.R;
 import com.example.cho1.guru2_final_project_1cho.bean.FleaBean;
+import com.example.cho1.guru2_final_project_1cho.db.FileDB;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -30,11 +32,14 @@ public class SellDetailActivity extends AppCompatActivity {
         TextView txtSellDetailPrice = findViewById(R.id.txtSellDetailPrice); //희망가
         TextView txtSellDetailOption = findViewById(R.id.txtSellDetailOption); //희망 옵션
 
-        //FleaBean fleaBean = mFleaList.get(i);
+        //FleaBean fleaBean = mFleaList;
 
         //상단 아이디 바 글쓴이 아이디, 올린 날짜 출력
         txtSellDetailId.setText(mFirebaseAuth.getUid());
         //txtSellDetailDate.setText(mFirebaseDB.getReference().child("memo").);
+
+        //상단 아이디(글쓴이 아이디)와 로그인 아이디가 같으면 수정, 삭제버튼 visibility 풀기
+        //if (TextUtils.equals(, mFirebaseAuth.getCurrentUser().getEmail()));
 
         /*
         * 1. 상단 아이디(글쓴이 아이디)와 로그인 아이디가 같으면 수정, 삭제버튼 visibility 풀기
