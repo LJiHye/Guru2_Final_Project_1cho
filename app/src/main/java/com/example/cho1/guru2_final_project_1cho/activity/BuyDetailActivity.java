440e93f96292ad2c148d482a13719805688f34e4
HeoYunJeong <yund_272@naver.com>
2019-07-17 16:14:14 +0900

    BuyDetailActivity 수정!! 완료!

--- app/src/main/java/com/example/cho1/guru2_final_project_1cho/activity/BuyDetailActivity.java
+++ app/src/main/java/com/example/cho1/guru2_final_project_1cho/activity/BuyDetailActivity.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 
 import com.example.cho1.guru2_final_project_1cho.R;
 import com.example.cho1.guru2_final_project_1cho.bean.FleaBean;
+import com.example.cho1.guru2_final_project_1cho.firebase.FleaAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -21,30 +22,41 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BuyDetailActivity extends AppCompatActivity {
     private TextView txtBuyDetailDate;
     private FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
     private FirebaseDatabase mFirebaseDB = FirebaseDatabase.getInstance();
 
     private FleaBean mFleaBean;
+    private ImageView imgDetail;
+    private TextView txtBuyDetailId, txtBuyDetailProduct, txtBuyDetailPrice, txtBuyDetailFinalPrice, txtBuyDetailState, txtBuyDetailFault, txtBuyDetailBuyDate, txtBuyDetailExpire, txtBuyDetailSize; //제품명
+
+    private List<FleaBean> mFleaList = new ArrayList<>();
+    private FleaAdapter mFleaAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_detail);
 
-        TextView txtBuyDetailId = findViewById(R.id.txtBuyDetailId); //아이디
-       txtBuyDetailDate = findViewById(R.id.txtBuyDetailDate);
+        mFleaBean = (FleaBean) getIntent().getSerializableExtra("ITEM");
 
-        ImageView imgDetail = findViewById(R.id.imgDetail); //이미지
-        TextView txtBuyDetailProduct = findViewById(R.id.txtBuyDetailProduct); //제품명
-        TextView txtBuyDetailPrice = findViewById(R.id.txtBuyDetailPrice); //정가
-        TextView txtBuyDetailFinalPrice = findViewById(R.id.txtBuyDetailFinalPrice); //판매가
-        TextView txtBuyDetailState = findViewById(R.id.txtBuyDetailState); //제품상태
-        TextView txtBuyDetailFault = findViewById(R.id.txtBuyDetailFault); //하자유무
-        TextView txtBuyDetailBuyDate = findViewById(R.id.txtBuyDetailBuyDate); //구매일
-        TextView txtBuyDetailExpire = findViewById(R.id.txtBuyDetailExpire); //유통기한
-        TextView txtBuyDetailSize = findViewById(R.id.txtBuyDetailSize); //실측사이즈
+        txtBuyDetailId = findViewById(R.id.txtBuyDetailId); //아이디
+        txtBuyDetailDate = findViewById(R.id.txtBuyDetailDate); //날짜
+
+        imgDetail = findViewById(R.id.imgDetail); //이미지
+        txtBuyDetailProduct = findViewById(R.id.txtBuyDetailProduct); //제품명
+        txtBuyDetailPrice = findViewById(R.id.txtBuyDetailPrice); //정가
+        txtBuyDetailFinalPrice = findViewById(R.id.txtBuyDetailFinalPrice); //판매가
+        txtBuyDetailState = findViewById(R.id.txtBuyDetailState); //제품상태
+        txtBuyDetailFault = findViewById(R.id.txtBuyDetailFault); //하자유무
+        txtBuyDetailBuyDate = findViewById(R.id.txtBuyDetailBuyDate); //구매일
+        txtBuyDetailExpire = findViewById(R.id.txtBuyDetailExpire); //유통기한
+        txtBuyDetailSize = findViewById(R.id.txtBuyDetailSize); //실측사이즈
 
         LinearLayout layoutVisibility = findViewById(R.id.layoutVisibility); //수정, 삭제 버튼 감싼 레이아웃
         Button btnModify = findViewById(R.id.btnModify);
@@ -63,40 +75,47 @@ public class BuyDetailActivity extends AppCompatActivity {
 //            layoutVisibility.setVisibility(View.VISIBLE);
 //        }
 
-        DatabaseReference userDBRef = FirebaseDatabase.getInstance().getReference();
-        userDBRef.child("buy").child(uuid).addValueEventListener(new ValueEventListener() {
+        mFirebaseDB.getReference().child("buy").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //데이터를 받아와서 List에 저장.
+                mFleaList.clear();
 
-                FleaBean fleaOption = dataSnapshot.child(uuid).getValue(FleaBean.class);
-                //txtBuyDetailDate.setText(fleaOption.date);
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    for (DataSnapshot snapshot2 : snapshot.getChildren()) {
+                        FleaBean bean = snapshot2.getValue(FleaBean.class);
+                        if (TextUtils.equals(bean.id, mFleaBean.id)) {
+                            txtBuyDetailProduct.setText(bean.title);
+                            txtBuyDetailPrice.setText(bean.price);
+                            txtBuyDetailFinalPrice.setText(bean.saleprice);
+                            txtBuyDetailState.setText(bean.state);
+                            txtBuyDetailFault.setText(bean.fault);
+                            txtBuyDetailBuyDate.setText(bean.buyday);
+                            txtBuyDetailExpire.setText(bean.expire);
+                            txtBuyDetailSize.setText(bean.size);
+
+                            txtBuyDetailDate.setText(bean.date);
+                            txtBuyDetailId.setText(bean.id);
+                        }
+                    }
+                }
+
+                if (mFleaAdapter != null) {
+                    mFleaAdapter.setList(mFleaList);
+                    mFleaAdapter.notifyDataSetChanged();
+                }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-
             }
         });
-
-        Intent intent = getIntent(); //던진 인텐트 받기
-        FleaBean fleaOption;
-
-        int index = intent.getIntExtra("INDEX", -1); //만약 인덱스를 못받았다면 -1을 보내도록 디폴트 값 지정
-        if (index != -1){
-            fleaOption = (FleaBean) intent.getSerializableExtra("ITEM");
-
-            //원본 Item을 UI에 적용
-//            imgDetail.setImageResource(fleaOption.());
-//            txtViewTitle.setText(fleaOption.getTitle());
-//            txtViewSubTitle.setText(fleaOption.getSubTitle());
-//            txtViewPrice.setText(fleaOption.getPrice());
-        }
-
-
-        /*
-         * 1. 상단 아이디(글쓴이 아이디)와 로그인 아이디가 같으면 수정, 삭제버튼 visibility 풀기
-         * 2. 기존에 올린 게시물에서 값 가져와서 setText
-         * 3. 댓글 구현 (db를 더 만들어야 하는가??, 뿌린다면 리스트로?)
-         * */
     }
+
+
+    /*
+     * 1. 상단 아이디(글쓴이 아이디)와 로그인 아이디가 같으면 수정, 삭제버튼 visibility 풀기
+     * 2. 기존에 올린 게시물에서 값 가져와서 setText
+     * 3. 댓글 구현 (db를 더 만들어야 하는가??, 뿌린다면 리스트로?)
+     * */
 }
