83760fd679921375d8be22a43d7b96b129c542ee
LJiHye <star4266@naver.com>
2019-07-19 13:06:25 +0900

    내가 쓴 글 카테고리별 전체 삭제

--- app/src/main/java/com/example/cho1/guru2_final_project_1cho/fragment/FragmentMySell.java
+++ app/src/main/java/com/example/cho1/guru2_final_project_1cho/fragment/FragmentMySell.java
@@ -1,17 +1,21 @@
 package com.example.cho1.guru2_final_project_1cho.fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.cho1.guru2_final_project_1cho.R;
+import com.example.cho1.guru2_final_project_1cho.activity.SellWriteActivity;
 import com.example.cho1.guru2_final_project_1cho.bean.FleaBean;
 import com.example.cho1.guru2_final_project_1cho.bean.MemberBean;
 import com.example.cho1.guru2_final_project_1cho.db.FileDB;
@@ -48,7 +52,48 @@ public class FragmentMySell extends Fragment {
         mSellAdapter = new MySellAdapter(getActivity(), mSellList);
         mLstMySell.setAdapter(mSellAdapter);
 
+        view.findViewById(R.id.btnDelMySell).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                delete();
+            }
+        });
+
         return view;
+    }
+
+    //삭제
+    private void delete() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("삭제");
+        builder.setMessage("삭제하시겠습니까?");
+        builder.setNegativeButton("아니오", null);
+        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String email = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+                String uuid = SellWriteActivity.getUserIdFromUUID(email);
+
+                mFirebaseDB.getReference().child("sell").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        //data가 바뀔 때마다 이벤트가 들어옴
+                        //data를 받아와서 List에 저장
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()){  //파이어베이스가 이중 구조여서
+                            FleaBean bean = snapshot.getValue(FleaBean.class);
+                            if(TextUtils.equals(mLoginMember.memId, bean.userId))
+                                FirebaseDatabase.getInstance().getReference().child("sell").child(bean.id).removeValue();
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+                Toast.makeText(getActivity(), "삭제 되었습니다.", Toast.LENGTH_LONG).show();
+            }
+        });
+        builder.create().show();
     }
 
     @Override
