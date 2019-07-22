647db45378e193763de7ffcdb8b6a414fa0bb869
Your Name <you@example.com>
2019-07-22 17:28:49 +0900

    무나 fragment 추가

---
+++ app/src/main/java/com/example/cho1/guru2_final_project_1cho/fragment/FragmentFreeShare.java
@@ -0,0 +1,46 @@
+package com.example.cho1.guru2_final_project_1cho.fragment;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import com.example.cho1.guru2_final_project_1cho.R;
+import com.example.cho1.guru2_final_project_1cho.activity.BuyWriteActivity;
+import com.example.cho1.guru2_final_project_1cho.activity.ExWriteActivity;
+import com.example.cho1.guru2_final_project_1cho.bean.ExBean;
+import com.example.cho1.guru2_final_project_1cho.bean.MemberBean;
+import com.example.cho1.guru2_final_project_1cho.db.FileDB;
+import com.example.cho1.guru2_final_project_1cho.firebase.ExAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FragmentFreeShare extends Fragment {
+    //layout fragment
+    public ListView mLstFree;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_free_share, container, false);
+
+        mLstFree = view.findViewById(R.id.lstFree);
+
+        return view;
+    }
+}
