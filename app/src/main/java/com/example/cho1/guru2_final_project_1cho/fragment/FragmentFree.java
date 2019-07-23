a0a2cc6c33cfb601152c89553a7b848aa0ff58a0
LJiHye <star4266@naver.com>
2019-07-23 10:56:10 +0900

    지도..

--- app/src/main/java/com/example/cho1/guru2_final_project_1cho/fragment/FragmentFree.java
+++ app/src/main/java/com/example/cho1/guru2_final_project_1cho/fragment/FragmentFree.java
@@ -1,33 +1,23 @@
 package com.example.cho1.guru2_final_project_1cho.fragment;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+
 import com.example.cho1.guru2_final_project_1cho.R;
-import com.example.cho1.guru2_final_project_1cho.activity.BuyWriteActivity;
-import com.example.cho1.guru2_final_project_1cho.activity.ExWriteActivity;
 import com.example.cho1.guru2_final_project_1cho.activity.FreeWriteActivity;
-import com.example.cho1.guru2_final_project_1cho.bean.ExBean;
-import com.example.cho1.guru2_final_project_1cho.bean.FleaBean;
 import com.example.cho1.guru2_final_project_1cho.bean.FreeBean;
-import com.example.cho1.guru2_final_project_1cho.bean.MemberBean;
-import com.example.cho1.guru2_final_project_1cho.db.FileDB;
-import com.example.cho1.guru2_final_project_1cho.firebase.BuyAdapter;
-import com.example.cho1.guru2_final_project_1cho.firebase.ExAdapter;
 import com.example.cho1.guru2_final_project_1cho.firebase.FreeAdapter;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
@@ -47,6 +37,11 @@ public class FragmentFree extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_free_share, container, false);
+
+        ActivityCompat.requestPermissions(getActivity(), new String[]{
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+        }, 0);
 
         mLstFree = view.findViewById(R.id.lstFree);
 
